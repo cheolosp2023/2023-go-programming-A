418ba5b33dc5cf73d8b8caffa5956c0552129009
cheolosp2023 <cheolosp2023@gmail.com>
2023-10-23 15:29:37 +0900

    week09 v1.75

--- week09/main.go
+++ week09/main.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
+func main() {
+	a := 10
+	//var b int = 20
+	var pa *int = &a
+
+	fmt.Printf("%T %T\n", &a, pa)
+	fmt.Printf("%x %x %x\n", &a, pa, &pa) //주소값 출력(pa와 &pa 다름)
+	fmt.Println(&a, pa, &pa)              //위와 같음(포인터는 메모리 주소값 출력)
+	fmt.Println(*pa)
+	pa = &b //b의 메모리 주소를 받음
+	fmt.Println(*pa)
+}
+
+/*
+package main
+
+import "fmt"
+
 func swap(n1 *int, n2 *int) {
+	//fmt.Println(n1, n2) //16진수의 주소값(포인터는 주소값 반환)
 	temp := *n1
 	*n1 = *n2
-	*n2 = temp //
+	*n2 = temp
 }
 
 func main() {
@@ -18,7 +37,7 @@ func main() {
 	swap(&a, &b)      //pass by pointer
 	fmt.Println(a, b) //포인터를 사용해서 a와 b의 순서 바꾸기
 }
-
+*/
 /*
 포인터 사용
 package main
